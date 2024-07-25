1610ee8bb7326f55e058301f6e530962705e285f
Anna Prado <annakaroline9298@gmail.com>
2024-07-25 16:57:42 -0300

    Hello world

---
+++ src/main/java/com/annagames/FiescApiApplication.java
@@ -0,0 +1,13 @@
+package com.annagames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiescApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiescApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/annagames/FiescApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.annagames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiescApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
